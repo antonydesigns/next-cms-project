ccde89b3613991faf3f19250e506397168904c51
gio <gio>
2023-11-28 16:33:34 +0800

    setup firebase

---
+++ app/(configs)/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBV9CApccRKF_ygbNQXaGkA4ghLg3a58dM",
+  authDomain: "next-cms-project.firebaseapp.com",
+  projectId: "next-cms-project",
+  storageBucket: "next-cms-project.appspot.com",
+  messagingSenderId: "400386546461",
+  appId: "1:400386546461:web:601081792172a338bd5ff4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
